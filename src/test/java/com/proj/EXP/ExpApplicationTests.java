97fb9fbf58ffba06afc911cfc9a435a278e08c0f
Cualone <june568@naver.com>
2024-04-16 17:07:20 +0900

    init

---
+++ src/test/java/com/proj/EXP/ExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.EXP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
