332d4377b80f4206c0d010ab057c0cde7ab8771b
Cualone <june568@naver.com>
2024-04-19 23:58:49 +0900

    테스트용 member init

--- src/main/java/com/proj/EXP/base/initData/Init.java
+++ src/main/java/com/proj/EXP/base/initData/Init.java
@@ -1,6 +1,8 @@
 package com.proj.EXP.base.initData;
 
 import com.proj.EXP.exercise.service.ExerciseService;
+import com.proj.EXP.member.entity.Member;
+import com.proj.EXP.member.service.MemberService;
 import com.proj.EXP.target.entity.Target;
 import com.proj.EXP.target.repository.TargetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,21 +10,24 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class Init {
 
-    @Autowired
-    private ExerciseService exerciseService;
-
-    @Autowired
-    private TargetRepository targetRepository;
-
     @Bean
-    CommandLineRunner initData() {
+    CommandLineRunner initData(
+            ExerciseService exerciseService,
+            TargetRepository targetRepository,
+            MemberService memberService
+    ) {
         return args -> {
+
+            memberService.create("1234", "1234", "123412", false, LocalDate.of(1999,1,27), 123, 123);
+
+
             List<Target> exerciseTargets = Arrays.asList(
                     new Target(1L, "가슴"),
                     new Target(2L, "등"),
@@ -32,11 +37,11 @@ public class Init {
             );
             targetRepository.saveAll(exerciseTargets);
 
-            exerciseService.create(null, exerciseTargets.get(0), "벤치프레스");
-            exerciseService.create(null, exerciseTargets.get(1), "데드리프트");
-            exerciseService.create(null, exerciseTargets.get(2), "사이드 레터럴 레이즈");
-            exerciseService.create(null, exerciseTargets.get(3), "바벨 컬");
-            exerciseService.create(null, exerciseTargets.get(4), "스쿼트");
+            exerciseService.create(null, exerciseTargets.get(0),true, null, "벤치프레스");
+            exerciseService.create(null, exerciseTargets.get(1),true, null, "데드리프트");
+            exerciseService.create(null, exerciseTargets.get(2),true, null, "사이드 레터럴 레이즈");
+            exerciseService.create(null, exerciseTargets.get(3),true, null, "바벨 컬");
+            exerciseService.create(null, exerciseTargets.get(4),true, null, "스쿼트");
         };
     }
 }
